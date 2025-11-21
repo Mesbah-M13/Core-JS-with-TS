36093db31bd61892ec92d95c2d81263203e087b8
Mesbah-M13 <mesbah0010@gmail.com>
2025-11-21 23:35:58 +0600

    add frontend to backend

--- data-fetching/backend/src/server.ts
+++ data-fetching/backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import chalk from "chalk";
+import cors from "cors"; // ⬅ important
 
 const app = express();
 
+app.use(cors()); // ⬅ allow requests from frontend
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from TypeScript Express!");
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend API...!" });
 });
 
 const PORT = 4600;
--- data-fetching/frontend/src/App.tsx
+++ data-fetching/frontend/src/App.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:4600/api/message")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <>
+      <h1>{message}</h1>
 
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
