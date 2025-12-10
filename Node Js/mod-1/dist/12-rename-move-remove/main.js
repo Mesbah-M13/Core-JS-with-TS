9fd440089f4b6cef22c97a0be049466395df49c7
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-10 08:01:01 +0600

    add move file to new dir

--- Node Js/mod-1/dist/12-rename-move-remove/main.js
+++ Node Js/mod-1/dist/12-rename-move-remove/main.js
@@ -11,16 +11,46 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //   if (err) throw err;
 //   console.log("New file created at: ", filePath);
 // });
-// now rename the new_file
+// ✅ now rename the new_file
+/*
 const fs = require("fs");
 const path = require("path");
-const oldPath = path.join(__dirname, "../../src/12-rename-move-remove/new_file.txt");
-const newPath = path.join(__dirname, "../../src/12-rename-move-remove/new_file_rename.txt");
+
+const oldPath = path.join(
+  __dirname,
+  "../../src/12-rename-move-remove/new_file.txt"
+);
+
+const newPath = path.join(
+  __dirname,
+  "../../src/12-rename-move-remove/new_file_rename.txt"
+);
+
 try {
-    fs.renameSync(oldPath, newPath);
-    console.log("File renamed successfully!", newPath);
+  fs.renameSync(oldPath, newPath);
+  console.log("File renamed successfully!", newPath);
+} catch (err) {
+  console.error("Error renaming file:", err);
+}
+*/
+// move file
+const fs = require("fs");
+const path = require("path");
+// old file path
+const oldPath = path.join(__dirname, "../../src/12-rename-move-remove/new_file_rename.txt");
+// new folder path
+const newDir = path.join(__dirname, "../../src/12-rename-move-remove/new_dir");
+// create folder if not exists
+if (!fs.existsSync(newDir)) {
+    fs.mkdirSync(newDir);
+}
+// new file path inside new_dir
+const newPath = path.join(newDir, "new_file_rename.txt");
+try {
+    fs.renameSync(oldPath, newPath); // move file
+    console.log("File moved successfully!", newPath);
 }
 catch (err) {
-    console.error("Error renaming file:", err);
+    console.error("Error moving file:", err);
 }
 //# sourceMappingURL=main.js.map
--- Node Js/mod-1/src/12-rename-move-remove/main.ts
+++ Node Js/mod-1/src/12-rename-move-remove/main.ts
@@ -13,8 +13,8 @@
 //   console.log("New file created at: ", filePath);
 // });
 
-// now rename the new_file
-
+// ✅ now rename the new_file
+/*
 const fs = require("fs");
 const path = require("path");
 
@@ -33,4 +33,34 @@ try {
   console.log("File renamed successfully!", newPath);
 } catch (err) {
   console.error("Error renaming file:", err);
+}
+*/
+
+// move file
+
+const fs = require("fs");
+const path = require("path");
+
+// old file path
+const oldPath = path.join(
+  __dirname,
+  "../../src/12-rename-move-remove/new_file_rename.txt"
+);
+
+// new folder path
+const newDir = path.join(__dirname, "../../src/12-rename-move-remove/new_dir");
+
+// create folder if not exists
+if (!fs.existsSync(newDir)) {
+  fs.mkdirSync(newDir);
+}
+
+// new file path inside new_dir
+const newPath = path.join(newDir, "new_file_rename.txt");
+
+try {
+  fs.renameSync(oldPath, newPath); // move file
+  console.log("File moved successfully!", newPath);
+} catch (err) {
+  console.error("Error moving file:", err);
 }
