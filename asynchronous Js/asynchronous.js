1c24106e5819d3d62fd543f0f925562c73661109
Mesbah-M13 <mesbah0010@gmail.com>
2025-12-21 00:07:06 +0600

    add callback function

--- asynchronous Js/asynchronous.js
+++ asynchronous Js/asynchronous.js
@@ -1,32 +1,41 @@
-function checkInventory() {
+function checkInventory(callback) {
   setTimeout(() => {
     console.log("Checking inventory");
+    callback();
   }, 1000);
 }
 
-function createOrder() {
+function createOrder(callback) {
   setTimeout(() => {
     console.log("Creating order");
+    callback();
   }, 3000);
 }
 
-function chargePayment() {
+function chargePayment(callback) {
   setTimeout(() => {
     console.log("Charging the payment");
+    callback();
   }, 2000);
 }
 
-function sendInvoice() {
+function sendInvoice(callback) {
   setTimeout(() => {
     console.log("Sending the invoice");
+    callback();
   }, 1000);
 }
 
 function main() {
-  checkInventory();
-  createOrder();
-  chargePayment();
-  sendInvoice();
+  checkInventory(() => {
+    createOrder(() => {
+      chargePayment(() => {
+        sendInvoice(() => {
+          console.log("All Done");
+        });
+      });
+    });
+  });
   console.log("Other requests are processing..."); // it will print 1st as others are showing result from asynchronous call
 }
 
